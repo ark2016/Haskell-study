1c56317f9f994474275366e0eb11e532b3a471d8
Arkadiy <91221701+ark2016@users.noreply.github.com>
2024-07-06 20:22:12 +0300

    Update TreeFunctor.hs

--- TreeFunctor.hs
+++ TreeFunctor.hs
@@ -1,3 +1,15 @@
+{-
+Определите представителя класса Functor для бинарного дерева, в каждом узле которого хранятся элементы типа Maybe:
+
+data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
+
+GHCi> words <$> Leaf Nothing
+Leaf Nothing
+
+GHCi> words <$> Leaf (Just "a b")
+Leaf (Just ["a","b"])
+
+-}
 import Data.Functor
 data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
 
